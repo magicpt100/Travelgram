44da5b298c2e16b56bbd383786f4254798623f70
Serena <zy16373@gmail.com>
2019-05-09 15:48:11 -0400

    add like and delete like

--- front_end/trip_list/assets/js/functions.js
+++ front_end/trip_list/assets/js/functions.js
@@ -6,10 +6,8 @@ function delete_node(el) {
 
   console.log($(el.parentNode));
 }
-
-function likeTrip(btn) {
-  if ($(btn).hasClass("liked") == false) {
-    // send like request
+function getUserNameByToken() {
+  if (window.location.href.includes('id_token')) {
     var id_token = url.searchParams.get("id_token");
     if (id_token == null) {
       id_token = location.hash.substring(1).split("&")[0].split("=")[1];
@@ -18,11 +16,38 @@ function likeTrip(btn) {
       id_token = "eyJraWQiOiJmelBrQXg3dkpCSlNNRmt5U3VMMkp1V2d2M2VpMkt2MGVBVkhmOVMzZnVFPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiTTA5SXVMeUhpc1BfTUdjbVh6SWJRZyIsInN1YiI6IjFhZmFkYjRhLTA1ZjgtNGIwZS1iNjkwLTIzZmE5YTJlZDZkNSIsImF1ZCI6IjQ5czd2amJodWlwODMxcWExMmc4cHZhNnJpIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNTU2OTk4MjgxLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9sTERZdERoakgiLCJjb2duaXRvOnVzZXJuYW1lIjoiR2lsYmVydFgiLCJleHAiOjE1NTcwMDE4ODEsImlhdCI6MTU1Njk5ODI4MSwiZW1haWwiOiJkd3lhbmVnaWxiZXJ0QGdtYWlsLmNvbSJ9.h3gtiXxxBOF_T73SbMR1EcrjxLcnDcOgVbP4tX9uHmiXI7CsC6arY4YYd33qEPbQ9m5BZfSho-l4foOh7k12NHzDrpJuLQaCNuAS5PYSTEwsjj-gekiP4Azc8Q_T39epL9PkPHUzfEGk38PViTOvfM5ZhH3SQDcLQoDH0nBpBpJVR0T6xUU8sTIKQHMx5QAyKMGaE4MwWxQwLBxqVcNq0jsQzzyrTdwWvHcUNvgZqHKz2GYPbmvfwjSh25ZHTv1zVztg4-tcyPPTamuY2-5qx4DW6AHv4L595ixNFEKAssKtuodbMdRv1nC1UtG7BuzHpJFdC9cNOatqV2-gx4Jj1w"
     }
     var username = parseJwt(id_token)["cognito:username"];
-    apigClient.favoriteTripPost({"TripID": $(btn.parentNode.parentNode).prop('id'), "UserName": username}, {"TripID": $(btn.parentNode.parentNode).prop('id'), "UserName": username}, {}).then(function (result) {
+    return username;
+}
+}
+function likeTrip(btn) {
+  var username = getUserNameByToken();
+  var tripid = $(btn.parentNode.parentNode.parentNode).prop('id')
+  if ($(btn).hasClass("liked") == false) {
+    // send like request
+
+    apigClient.favoriteTripPost({"TripID": parseInt(tripid), "UserName": username, }, {"TripID": parseInt(tripid), "UserName": username}, {headers:{"Authorization": id_token}}).then(function (result) {
         $(btn).addClass("liked");
+        var numLike = $(btn.parentNode.parentNode.parentNode).find('.like-span').html()
+        console.log(numLike)
+        numLike = parseInt(numLike) + 1
+        numLike = numLike.toString()
+        $(btn.parentNode.parentNode.parentNode).find('.like-span').html(numLike)
 
     }).catch(function (error) {
-
+      console.log(error)
+    })
+  } else {
+    // cancel like
+        console.log(id_token)
+    apigClient.favoriteTripTripIDUserNameDelete({'UserName':username, "TripID": parseInt(tripid)}, null, {headers:{"Authorization": id_token}}).then(function(result) {
+      $(btn).removeClass("liked");
+      var numLike = $(btn.parentNode.parentNode.parentNode).find('.like-span').html()
+      console.log(numLike)
+      numLike = parseInt(numLike) - 1
+      numLike = numLike.toString()
+      $(btn.parentNode.parentNode.parentNode).find('.like-span').html(numLike)
+    }).catch(function (error) {
+      console.log(error);
     })
   }
 }
@@ -95,6 +120,8 @@ function tripDetail(trip) {
         this.id = arg.TripID;
         this.cover = arg.CoverPhoto;
         this.numLikes = arg.NumLikes;
+        this.username = arg.Username;
+        this.liked = arg.isLikeByUser;
         if (arg.isLikeByUser == true) {
           this.like = true;
         } else{
@@ -120,8 +147,9 @@ function tripDetail(trip) {
                   //$tripItem.find('.button-like').hide();
                   $tripItem.find('.button-like').attr("disabled", true);
                 } else {
-                    console.log(this.numLikes)
-                    console.log($tripItem.find('.date').html())
+                    if (this.liked) {
+                      $tripItem.find('.button-like').addClass("liked")
+                    }
                 }
                 $tripItem.attr('id', this.id);
                 $tripItem.find('.cover').attr("src",this.cover);
@@ -180,7 +208,8 @@ function tripDetail(trip) {
       tripList.clear()
       var apigClient = apigClientFactory.newClient();
       console.log("load trips");
-      apigClient.tripsGet()
+      var userName = getUserNameByToken();
+      apigClient.tripsGet({}, null, {headers:{'userName':userName}})
         .then(function(result){
           var trips = result.data;
           console.log(trips);
@@ -199,6 +228,8 @@ function tripDetail(trip) {
           var tmp1 = document.getElementById('tmp1');
           tmp2.parentNode.removeChild(tmp2);
           tmp1.parentNode.removeChild(tmp1);
+        }).catch(function(error) {
+          console.log(error);
         });
     }
 
